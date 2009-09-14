396d24e79c8e4ab24376e8ad92d2fd484e7bc6b1
Alexander Ivanov <alexander.ivanov@redsolution.ru>
2009-09-14 16:05:19 +0600

    Add posibility to set source and result file names for trusted_log

--- trustedhtml/management/commands/trusted_log.py
+++ trustedhtml/management/commands/trusted_log.py
@@ -5,25 +5,35 @@ from trustedhtml.models import Log
 from trustedhtml.utils import get_lined
 
 class Command(BaseCommand):
-    help = '''Usage: manage.py trusted_log
+    help = '''Usage: manage.py trusted_log [source-file-name] [result-file-name]
 
 Make files with source and result text of html validation.
-For each record will be created two files:
-    <id>s.txt - with source text,
-    <id>r.txt - with result text.
+
+source-file-name -  is template to generate file name for source text
+                    by default: '%ds.txt'
+result-file-name - is template to generate file name for source text
+                    by default: '%dr.txt'
 '''
 
     def handle(self, *args, **options):
-        if args:
+        if len(args) > 2:
             print self.help
             return
+        try:
+            source = args[0]
+        except IndexError:
+            source = '%06ds.txt'
+        try:
+            result = args[1]
+        except IndexError:
+            result = '%06dr.txt'
         lst = Log.objects.all()
         if not lst:
             print 'There is no elements in log.'
             return
         for obj in lst:
-            print '%ds.txt' % obj.id,
-            open('%ds.txt' % obj.id, 'w').write(get_lined(obj.source.encode('utf-8')))
-            print '%dr.txt' % obj.id,
-            open('%dr.txt' % obj.id, 'w').write(get_lined(obj.result.encode('utf-8')))
+            print source % obj.id,
+            open(source % obj.id, 'w').write(get_lined(obj.source.encode('utf-8')))
+            print result % obj.id,
+            open(result % obj.id, 'w').write(get_lined(obj.result.encode('utf-8')))
         print '\nDone.'
