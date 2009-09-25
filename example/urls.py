e319eae10b71e7cc7d0faca28a5ec791dde4a9ce
Alexander Ivanov <alexander.ivanov@redsolution.ru>
2009-09-25 19:00:10 +0600

    Fix example/urls.py

--- example/urls.py
+++ example/urls.py
@@ -6,4 +6,4 @@ for view in ['response', 'notfound', 'error', 'redirect_response',
     'redirect_notfound', 'redirect_redirect_response', 'redirect_cicle', 
     'permanent_redirect_response', 'http404', 'http500', 
     'request_true_response', 'request_false_response',]:
-    urlpatterns += patterns('example.views', url('^%s' % view, view, name=view))
+    urlpatterns += patterns('example.views', url('^%s$' % view, view, name=view))
