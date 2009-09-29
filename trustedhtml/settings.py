9ae36330b80a65fc22b9f5b4252b5223798e685d
Alexander Ivanov <alexander.ivanov@redsolution.ru>
2009-09-29 16:09:36 +0600

    Add settings file

---
+++ trustedhtml/settings.py
@@ -0,0 +1,27 @@
+from django.conf import settings
+
+TRUSTEDHTML_ENABLE_LOG = getattr(settings, 'TRUSTEDHTML_ENABLE_LOG', False)
+
+TRUSTEDHTML_LINK_SITES = getattr(settings, 'TRUSTEDHTML_LINK_SITES', True)
+TRUSTEDHTML_IMAGE_SITES = getattr(settings, 'TRUSTEDHTML_IMAGE_SITES', True)
+TRUSTEDHTML_OBJECT_SITES = getattr(settings, 'TRUSTEDHTML_OBJECT_SITES', True)
+
+TRUSTEDHTML_ALLOW_SCHEMES = getattr(settings, 'TRUSTEDHTML_ALLOW_SCHEMES', [
+    'http', 'https', 'shttp', 'ftp', 'sftp', 'file', 'mailto',  
+    'svn', 'svn+ssh', 'telnet', 'mms', 'ed2k', ])
+
+TRUSTEDHTML_CUT_SITES = getattr(settings, 'TRUSTEDHTML_CUT_SITES', False)
+TRUSTEDHTML_CUT_SCHEMES = getattr(settings, 'TRUSTEDHTML_CUT_SCHEMES', [
+    'http', ])
+
+TRUSTEDHTML_VERIFY_SITES = getattr(settings, 'TRUSTEDHTML_VERIFY_SITES', False)
+TRUSTEDHTML_VERIFY_SCHEMES = getattr(settings, 'TRUSTEDHTML_VERIFY_SCHEMES', [
+    'http', 'https', 'ftp', ],)
+
+TRUSTEDHTML_VERIFY_LOCAL = getattr(settings, 'TRUSTEDHTML_VERIFY_LOCAL', False)
+
+TRUSTEDHTML_LOCAL_SITES = getattr(settings, 'TRUSTEDHTML_LOCAL_SITES', False)
+TRUSTEDHTML_LOCAL_SCHEMES = getattr(settings, 'TRUSTEDHTML_LOCAL_SCHEMES', [
+    'http', ],)
+
+TRUSTEDHTML_VERIFY_USER_AGENT = getattr(settings, 'TRUSTEDHTML_VERIFY_USER_AGENT', 'TrustedHtml')
